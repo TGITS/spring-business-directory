ca07a122dd746ccb0bf218002aa1d32a17d32263
TGITS <christophe.vaudry@gmail.com>
2015-12-10 13:56:07 +0100

    Finalisation de l'enumération, commité avant d'être finie...

--- project/spring-business-directory-domain/src/main/java/business/directory/domain/core/CoordinateType.java
+++ project/spring-business-directory-domain/src/main/java/business/directory/domain/core/CoordinateType.java
@@ -4,13 +4,19 @@ package business.directory.domain.core;
  * Created by cvaudry on 10/12/2015.
  */
 public enum CoordinateType {
-    private String name;
 
-    CoordinateType(String name) {
-        this.name = name;
+    PHONE("Phone"),
+    EMAIL("Email"),
+    MOBILE("Mobile"),
+    POSTAL("Postal");
+
+    private String type;
+
+    CoordinateType(String type) {
+        this.type = type;
     }
 
-    public String name() {
-        return name;
+    public String type() {
+        return type;
     }
 }
