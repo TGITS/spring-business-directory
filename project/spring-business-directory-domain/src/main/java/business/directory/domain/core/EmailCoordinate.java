cf00e6f3d6c3621bfdea3b2b58df333ae1d1190b
TGITS <thegeekintheshell@gmail.com>
2015-12-31 14:25:13 +0100

    Adding the EmailCoordinate class

---
+++ project/spring-business-directory-domain/src/main/java/business/directory/domain/core/EmailCoordinate.java
@@ -0,0 +1,26 @@
+package business.directory.domain.core;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * Created by Poulon on 31/12/2015.
+ */
+@EqualsAndHashCode
+@ToString
+public class EmailCoordinate extends AbstractCoordinate {
+
+    private final String pattern = "\\w+@\\w+\\.\\w+";
+
+    public CoordinateType type() {
+        return CoordinateType.EMAIL;
+    }
+
+    protected String pattern() {
+        return pattern;
+    }
+
+    protected String exceptionMessage() {
+        return getValue() + " is not a valid email";
+    }
+}
